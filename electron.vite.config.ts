7420dfd32e1ea0cafc70fc121c52bf77fe1ff8ca
Wang Jian <wangjian0504@gmail.com>
2026-09-17 21:33:12 +0800

    feat: integrate shadcn ui

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'electron-vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -8,9 +9,10 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
+        '@': resolve('src/renderer/src'),
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react(), tailwindcss()]
   }
 })
